6217879ef16621c544d509546752dd3be328f558
Christian Deinert <ChDeinert@gmail.com>
2018-07-28 23:01:19 +0200

    Add basic implementation

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ customElements.define(
     constructor() {
       super();
       const wrapperTemplate = document.querySelector('#bware-of-cookies-template');
+
+      this.acknowledgedEvent = new CustomEvent('boc-accept', { bubbles: true });
+
+      this.clickHandler = (event) => {
+        this.shadowRoot.querySelector('.boc').className += ' boc--hidden';
+        this.dispatchEvent(this.acknowledgedEvent);
+      }
+
       const shadowRoot = this.attachShadow({ mode: 'open' })
         .appendChild(wrapperTemplate.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+      const button = this.shadowRoot.querySelector('.boc__btn');
+      button.addEventListener('click', this.clickHandler);
     }
   }
 );
